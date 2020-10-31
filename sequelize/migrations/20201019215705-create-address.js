aad695cd12c31444a75dcb812243f4f54e494672
Zac Littleberry <zaclittleberry@gmail.com>
2020-10-31 18:50:38 -0400

    process searchables and get by searchables on corps

---
+++ sequelize/migrations/20201019215705-create-address.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Addresses', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      CorpId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Corpsearches', key: 'id' }
+      },
+      source: {
+        type: Sequelize.ENUM('details', 'officers')
+      },
+      position: {
+        type: Sequelize.INTEGER
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      searchableAddressV1: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Addresses');
+  }
+};
